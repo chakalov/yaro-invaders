56db9299119aa90d7a8dcce8eca057b0dd81e02c
lordatrelios@gmail.com <lordatrelios@gmail.com@996e8f34-70d7-745a-9bd1-2b4c325549f9>
2012-05-02 21:43:26 +0000

    removing unwanted/unnecessary changes did earlier

--- trunk/Ships/PlayerShip.py
+++ trunk/Ships/PlayerShip.py
@@ -12,17 +12,15 @@ class PlayerShip(Ships.BaseShip.BaseShip):
 		self.slowed = False
 		self.shield = shield
 		self.health = maxHealth
-		self.dead = False
 	
 	def canShoot(self):
-		return self.stunned == False and Ships.BaseShip.BaseShip.canShoot(self) and self.dead == False
+		return self.stunned == False and Ships.BaseShip.BaseShip.canShoot(self)
 	
 	def takeDamage(self, damage):
 		self.health -= damage
 		if self.health <= 0:
 			if self.lives <= 0:
 				self.kill()
-				self.dead = True
 			else:
 				self.lives -= 1
 				self.health = self.maxHealth
